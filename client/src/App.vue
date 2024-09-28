7bf19e7aca73837cf11a3a943671c265dfe5c561
yodsapon64 <yodsaponb64@nu.ac.th>
2024-09-28 19:41:42 +0700

    Decorate and add interesting features in App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <navigationBar/>
-    <router-view/>
+    <navigationBar />
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
 
@@ -16,8 +16,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
-  /* margin-top: 60px; */
 }
 </style>
